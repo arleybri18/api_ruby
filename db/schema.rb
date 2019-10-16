98febd19851a2a3a268497e7ef69004b4a72a559
Yony Brinez <arleybri18@gmail.com>
2019-10-16 02:48:54 -0500

    Execute migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_071328) do
+ActiveRecord::Schema.define(version: 2019_10_16_074433) do
+
+  create_table "executions", force: :cascade do |t|
+    t.integer "state"
+    t.integer "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_executions_on_task_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "url"
+    t.integer "step_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["step_id"], name: "index_pages_on_step_id"
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.string "name"
+    t.string "info_step"
+    t.integer "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_steps_on_task_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "enabled"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
@@ -21,4 +56,8 @@ ActiveRecord::Schema.define(version: 2019_10_16_071328) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "executions", "tasks"
+  add_foreign_key "pages", "steps"
+  add_foreign_key "steps", "tasks"
+  add_foreign_key "tasks", "users"
 end
